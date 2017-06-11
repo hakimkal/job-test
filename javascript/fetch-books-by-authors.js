b2b75ea7d1aa5f67376a81a8a21e0cf3bfd4c695
Abdulhakim Haliru <hakimkal@gmail.com>
2017-06-12 00:35:53 +0100

    completed order book by ratings and fetch books by author

---
+++ javascript/fetch-books-by-authors.js
@@ -0,0 +1,25 @@
+/* 
+Write a function which accepts an author and returns books by that author in a file name
+ “javascript/fetch-books-by-authors.js”
+Push both codes to your copy of the repository
+
+*/
+import books from './books';
+
+let getBooksByAuthor= (author)=>{
+let booksByAuthor = [];
+books.filter(book=>{
+    if(book.author == author){
+
+    
+    booksByAuthor.push(book) ;
+}
+
+  
+});
+return booksByAuthor;  
+};
+
+
+ getBooksByAuthor("George Orwell");
+
---
+++ javascript/order-books-by-ratings.js
@@ -0,0 +1,21 @@
+/*
+Task:
+Write a function which accepts a collection of books and orders the books by 
+ratings; Highest ratings first, lowest ratings last in a file named “javascript/order-books-by-ratings.js”
+
+
+ */
+
+import books from './books';
+
+
+let orderBooksByRatings = books =>{
+   
+  /* Sort array by  ratings */ 
+
+books.sort((a,b)=>{return (b.ratings - a.ratings)});
+console.log(books);
+
+};   
+
+orderBooksByRatings(books);
